74bee77f3160a725f294041aab39d8505fe2550f
jenny19919215 <jenny19919215@gmail.com>
2023-08-22 09:10:07 +0200

    https://www.youtube.com/watch?v=FXpIoQ_rT_c&t=7793s&ab_channel=freeCodeCamp.org
    demo finished.

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,29 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Home from '../views/Home.vue'
+import Products from '../views/Products.vue'
+import PastOrders from '../views/PastOrders.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: Home
   },
   {
-    path: '/about',
-    name: 'about',
+    path: '/products',
+    name: 'Products',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: Products
+  },
+  {
+    path: '/past-orders',
+    name: 'PastOrders',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: PastOrders
   }
 ]
 
